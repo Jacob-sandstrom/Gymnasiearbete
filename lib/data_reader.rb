ccd24ff702e1aab667e3c0f462f0bee49afd885e
itggot-jacob-sandstrom <jacob.sandstrom@elev.ga.ntig.se>
2019-09-22 21:35:11 +0200

    .

--- lib/data_reader.rb
+++ lib/data_reader.rb
@@ -1,15 +1,21 @@
+require 'sqlite3'
 
 
 class Data_reader
 
 
+    def self.read 
+        db = connect
+        data = access_data(db)
+
+    end
 
     def self.connect
         SQLite3::Database.new '../db/Tile_symbols.db'
     end
 
-    def self.access_data
-
+    def self.access_data(db)
+        data = []
 
     end
 
