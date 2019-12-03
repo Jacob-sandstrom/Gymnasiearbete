c355e6d7fdca0fe362b37a03c2f3bb44a095c711
itggot-jacob-sandstrom <jacob.sandstrom@elev.ga.ntig.se>
2019-12-03 12:35:10 +0100

    .

--- lib/game.rb
+++ lib/game.rb
@@ -1,1 +1,43 @@
 
+require 'gosu'
+require 'yaml'
+require 'wisper'
+
+d = Dir.glob("*.rb")
+d.each do |dir|
+    unless dir == "editor.rb"
+        require_relative dir
+    end
+end
+
+
+class Editor < Gosu::Window
+    attr_accessor :currently_editing
+
+    def initialize
+        width = 1920  
+        height = 1080
+        super width, height, fullscreen:true
+        
+        @tilesize = 32
+
+        @camera = Camera.new(self)
+
+        @tile_map = YAML.load(File.read("../maps/tilemaps/map.yaml")) 
+        @object_map = YAML.load(File.read("../maps/objectmaps/map.yaml"))
+        @map_drawer = Map_drawer.new(self, @tilesize)
+    end
+
+
+    def update
+
+    end
+
+    def draw
+
+    end
+
+
+
+end
+
